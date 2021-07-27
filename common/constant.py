7b0976d267e35793cdd98844f912d0023e8d2e97
MadanStha7 <madanstha.124@gmail.com>
2021-07-27 14:23:13 +0545

    [add] grade choices

--- common/constant.py
+++ common/constant.py
@@ -25,3 +25,16 @@ DAY = (
 
 
 SYSTEM_DEFAULT_PASSWORD = "98HACOLLEGE123!@#"
+
+GRADE_CHOICES = (
+    (1, "One"),
+    (2, "Two"),
+    (3, "Three"),
+    (4, "Four"),
+    (5, "Five"),
+    (6, "Six"),
+    (7, "Seven"),
+    (8, "Eight"),
+    (9, "Nine"),
+    (10, "Ten"),
+)
