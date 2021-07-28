f4b3e311d9525776904703470fb44bb532dedfdc
reebika <bhattareebika@gmail.com>
2021-07-28 09:57:27 +0545

    [Create] subject and subject group model

--- academics/models.py
+++ academics/models.py
@@ -19,7 +19,6 @@ class Section(CommonInfo):
 
 
 class Grade(CommonInfo):
-
     name = models.CharField(max_length=20, choices=GRADE_CHOICES)
 
     class Meta:
@@ -48,6 +47,54 @@ class Faculty(CommonInfo):
     def save(self, *args, **kwargs):
         self.name = self.name.title()
         return super(Section, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Subject(CommonInfo):
+    """
+    model to store subjects
+    """
+
+    SUBJECT_TYPES = (("1", "Theory"), ("2", "Practical"), ("3", "Theory and Practical"))
+
+    name = models.CharField(max_length=50)
+    credit_hour = models.FloatField()
+    subject_code = models.CharField(max_length=10)
+    subject_type = models.CharField(max_length=1, choices=SUBJECT_TYPES)
+
+    class Meta:
+        db_table = "academics_subject"
+        unique_together = ["name", "institution"]
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        return super(Subject, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class SubjectGroup(CommonInfo):
+    name = models.CharField(max_length=50)
+    section = models.ManyToManyField(Section, related_name="subject_group", blank=True)
+    grade = models.ForeignKey(
+        Grade, related_name="subject_group", on_delete=models.CASCADE
+    )
+    faculty = models.ForeignKey(
+        Faculty, related_name="subject_group", on_delete=models.CASCADE
+    )
+    subject = models.ManyToManyField(Subject, related_name="subjects")
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = "academics_subject_group"
+        unique_together = ["name", "institution"]
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        return super(SubjectGroup, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name}"
