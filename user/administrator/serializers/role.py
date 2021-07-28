bea8686286a009494f6eacf9fb0311d3b828390f
RazAce Pudasaini <pudasainirajesh504@gmail.com>
2021-07-28 13:54:40 +0545

    [fix] the username field

--- user/administrator/serializers/role.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from user.models import Role
-
-
-class RoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        fields = ("title", "description", "institution")
---
+++ user/urls.py
@@ -0,0 +1,3 @@
+from user.common.urls.core import urlpatterns as common_urls
+
+urlpatterns = common_urls
