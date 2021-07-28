de8cb042a588f2e514620df9f9a39f8fe5d112af
reebika <bhattareebika@gmail.com>
2021-07-28 09:50:37 +0545

    [Add] subject and subjectgroup url in core

--- academics/administrator/urls/core.py
+++ academics/administrator/urls/core.py
@@ -1,3 +1,7 @@
 from .section import urlpatterns as section_urls
+from .subject import urlpatterns as subject_urls
+from .subject_group import urlpatterns as subject_group_urls
 
 urlpatterns = section_urls
+urlpatterns += subject_urls
+urlpatterns += subject_group_urls
