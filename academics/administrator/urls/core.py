708c3998669901409a9ea520efd47932f4d65f35
reebika <bhattareebika@gmail.com>
2021-07-28 13:18:48 +0545

    [Add] student urls in cor

--- academics/administrator/urls/core.py
+++ academics/administrator/urls/core.py
@@ -1,4 +1,6 @@
 from .section import urlpatterns as section_urls
 from .grade import urlpatterns as grade_urls
+from .subject import urlpatterns as subject_urls
 
 urlpatterns = section_urls + grade_urls
+urlpatterns += subject_urls
