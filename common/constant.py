49b8e12a9b976ef4293f3ac962c52c4e723ac9bc
MadanStha7 <madanstha.124@gmail.com>
2021-07-28 11:32:06 +0545

    [refactor] grade choices

--- common/constant.py
+++ common/constant.py
@@ -27,14 +27,6 @@ DAY = (
 SYSTEM_DEFAULT_PASSWORD = "98HACOLLEGE123!@#"
 
 GRADE_CHOICES = (
-    ("1", "One"),
-    ("2", "Two"),
-    ("3", "Three"),
-    ("4", "Four"),
-    ("5", "Five"),
-    ("6", "Six"),
-    ("7", "Seven"),
-    ("8", "Eight"),
-    ("9", "Nine"),
-    ("10", "Ten"),
+    ("11", "Eleven"),
+    ("12", "Twelve"),
 )
