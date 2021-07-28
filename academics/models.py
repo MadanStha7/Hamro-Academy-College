1f393687213c04c19d1b46ed8e3a7d5a9c7b7e3f
reebika <bhattareebika@gmail.com>
2021-07-28 12:12:06 +0545

    [Create] subject model

--- academics/models.py
+++ academics/models.py
@@ -8,7 +8,7 @@ class Section(CommonInfo):
 
     class Meta:
         db_table = "academics_section"
-        unique_together= ['name','institution']
+        unique_together = ["name", "institution"]
 
     def save(self, *args, **kwargs):
         self.name = self.name.title()
@@ -24,12 +24,35 @@ class Grade(CommonInfo):
 
     class Meta:
         db_table = "academics_grade"
-        unique_together= ['name','institution']
-        
+        unique_together = ["name", "institution"]
 
     def save(self, *args, **kwargs):
         self.name = self.name.title()
         return super(Grade, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Subject(CommonInfo):
+    """
+    model to store subjects
+    """
+
+    SUBJECT_TYPES = (("1", "Theory"), ("2", "Practical"), ("3", "Theory and Practical"))
+
+    name = models.CharField(max_length=50)
+    credit_hour = models.FloatField()
+    subject_code = models.CharField(max_length=10)
+    subject_type = models.CharField(max_length=1, choices=SUBJECT_TYPES)
+
+    class Meta:
+        db_table = "academic_subject"
+        unique_together = ["name", "institution"]
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        return super(Subject, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name}"
