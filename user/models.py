088bfb6262e538678c9ba7c447f3573d148501ac
RazAce Pudasaini <pudasainirajesh504@gmail.com>
2021-07-28 13:51:13 +0545

    [update] username field to phone

--- user/models.py
+++ user/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.apps import apps
+from django.contrib.auth.hashers import make_password
 from common.models import SystemRole
 from django.contrib.auth.models import AbstractUser, Group
 from uuid import uuid4
-from django.contrib.auth.models import Permission, GroupManager
+from django.contrib.auth.models import Permission, GroupManager, UserManager
 from core.models import InstitutionInfo
 from django.utils.translation import gettext_lazy as _
 
@@ -26,6 +28,42 @@ class Role(models.Model):
         unique_together = ["title", "institution"]
 
 
+class SystemUserManager(UserManager):
+    def _create_user(self, phone, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not phone:
+            raise ValueError("The given phone must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        phone = GlobalUserModel.normalize_username(phone)
+        user = self.model(phone=phone, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(phone, email, password, **extra_fields)
+
+    def create_superuser(self, phone, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(phone, email, password, **extra_fields)
+
+
 class SystemUser(AbstractUser):
     """
     extending the user info with general info as external field
@@ -40,7 +78,8 @@ class SystemUser(AbstractUser):
         null=True,
         blank=True,
     )
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15, unique=True)
+    username = None
     roles = models.ManyToManyField(
         Role,
         verbose_name=_("roles"),
@@ -52,3 +91,5 @@ class SystemUser(AbstractUser):
         related_name="user_roles",
         related_query_name="roles",
     )
+    USERNAME_FIELD = "phone"
+    objects = SystemUserManager()
