f2bc62de7b43795b3d03190b862105f33dff52fb
viperthapa <thaparam78666@gmail.com>
2021-07-29 10:33:58 +0545

    [fix] merge conflict

--- academics/administrator/urls/core.py
+++ academics/administrator/urls/core.py
@@ -1,6 +1,5 @@
 from .section import urlpatterns as section_urls
-from .grade import urlpatterns as grade_urls
 from .faculty import urlpatterns as faculty_urls
 
-
-urlpatterns = section_urls + grade_urls + faculty_urls
+urlpatterns = section_urls
+urlpatterns += faculty_urls
