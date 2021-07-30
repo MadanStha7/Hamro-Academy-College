8f1dcaee486743008e86f58d4b1a14bdfebd8058
viperthapa <thaparam78666@gmail.com>
2021-07-30 13:59:36 +0545

    [fix] concatenate section_urls and faculty_urls

--- academics/administrator/urls/core.py
+++ academics/administrator/urls/core.py
@@ -1,5 +1,4 @@
 from .section import urlpatterns as section_urls
 from .faculty import urlpatterns as faculty_urls
 
-urlpatterns = section_urls
-urlpatterns += faculty_urls
+urlpatterns = section_urls + faculty_urls
