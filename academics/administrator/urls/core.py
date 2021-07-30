2d73968fbe05519eb6e2cd0c21aac98efa955242
MadanStha7 <madanstha.124@gmail.com>
2021-07-30 10:58:14 +0545

    [fix] grade urls

--- academics/administrator/urls/core.py
+++ academics/administrator/urls/core.py
@@ -1,6 +1,8 @@
 from .section import urlpatterns as section_urls
 from .grade import urlpatterns as grade_urls
 from .faculty import urlpatterns as faculty_urls
+from .shift import urlpatterns as shift_urls
+from .class_url import urlpatterns as class_urls
 
 
-urlpatterns = section_urls + grade_urls + faculty_urls
+urlpatterns = section_urls + grade_urls + faculty_urls + shift_urls + class_urls
