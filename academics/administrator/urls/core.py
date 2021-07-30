f4eccd570714f814514394fa76809e3e38877f66
reebika <bhattareebika@gmail.com>
2021-07-30 14:15:27 +0545

    [Fix] core urls

--- academics/administrator/urls/core.py
+++ academics/administrator/urls/core.py
@@ -3,6 +3,16 @@ from .grade import urlpatterns as grade_urls
 from .faculty import urlpatterns as faculty_urls
 from .shift import urlpatterns as shift_urls
 from .class_url import urlpatterns as class_urls
+from .subject import urlpatterns as subject_urls
+from .subject_group import urlpatterns as subject_group_urls
 
 
-urlpatterns = section_urls + grade_urls + faculty_urls + shift_urls + class_urls
+urlpatterns = (
+    section_urls
+    + grade_urls
+    + faculty_urls
+    + shift_urls
+    + class_urls
+    + subject_urls
+    + subject_group_urls
+)
