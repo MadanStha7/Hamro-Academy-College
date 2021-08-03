2f6bdfc78de8013fc4476ffdd5ebd9badd4d8cda
reebika <bhattareebika@gmail.com>
2021-08-03 16:47:11 +0545

    [Add] student info urls in core

--- student/administator/urls/core.py
+++ student/administator/urls/core.py
@@ -1,3 +1,14 @@
-from .guardian import urlpatterns as guardian_urls
+# from .guardian import urlpatterns as guardian_urls
+from .student import urlpatterns as student_urls
+from .category import urlpatterns as category_urls
+from .student_academic import urlpatterns as student_academic_urls
+from .previous_academic import urlpatterns as previous_academic_urls
+from .document import urlpatterns as document_urls
 
-urlpatterns = guardian_urls
+urlpatterns = (
+    student_urls
+    + category_urls
+    + student_academic_urls
+    + previous_academic_urls
+    + document_urls
+)
