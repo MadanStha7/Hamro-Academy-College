9fc648863f8b5fa4c02db38d7a6b6e71c4806a7b
reebika <bhattareebika@gmail.com>
2021-08-03 16:46:26 +0545

    [Add] studentinfo serializer

--- student/administator/serializer/student.py
+++ student/administator/serializer/student.py
@@ -4,8 +4,9 @@ from django.db import transaction
 from rest_framework import serializers
 
 from common.constant import SYSTEM_DEFAULT_PASSWORD
+from common.utils import to_internal_value
 from student.models import StudentInfo
-from user.administrator.serializers.user import UserSerializer
+from user.common.serializers.user import UserSerializer
 
 
 class StudentListInfoSerializer(serializers.ModelSerializer):
@@ -14,9 +15,7 @@ class StudentListInfoSerializer(serializers.ModelSerializer):
     guardian_first_name = serializers.CharField(read_only=True)
     guardian_last_name = serializers.CharField(read_only=True)
     guardian_phone_number = serializers.CharField(read_only=True)
-    section = serializers.CharField(read_only=True)
     roll_number = serializers.CharField(read_only=True)
-    grade = serializers.CharField(read_only=True)
     blood_group = serializers.CharField(source="get_blood_group_display")
 
     class Meta:
@@ -27,12 +26,10 @@ class StudentListInfoSerializer(serializers.ModelSerializer):
             "student_first_name",
             "student_last_name",
             "phone_number",
-            "student_photo",
+            "photo",
             "guardian_first_name",
             "guardian_last_name",
             "guardian_phone_number",
-            "grade",
-            "section",
             "roll_number",
             "address",
             "blood_group",
@@ -40,33 +37,38 @@ class StudentListInfoSerializer(serializers.ModelSerializer):
 
 
 class StudentInfoSerializer(serializers.ModelSerializer):
-    student_user = UserSerializer()
-    category_name = serializers.CharField(
-        source="student_category.name", read_only=True
+    photo = serializers.SerializerMethodField(
+        read_only=True, required=False, allow_null=True
     )
-    blood_group_display = serializers.CharField(
-        source="get_blood_group_display", read_only=True
-    )
+    user = UserSerializer()
+    category_name = serializers.CharField(read_only=True)
+
+    def get_photo(self, obj):
+        return obj.photo.url if obj.photo else None
 
     class Meta:
         model = StudentInfo
-        read_only_fields = ["institution", "admission_number"]
+        read_only_fields = [
+            "institution",
+            "created_by",
+            "created_on",
+            "admission_number",
+        ]
         fields = [
             "id",
             "photo",
             "admission_number",
             "user",
-            "address",
+            "permanent_address",
+            "temporary_address",
             "dob",
             "gender",
+            "marital_status",
+            "spouse_name",
             "blood_group",
-            "blood_group_display",
-            "phone",
+            "religion",
             "student_category",
             "category_name",
-            "religion",
-            "medical_history",
-            "transportation_detail",
             "guardian_detail",
             "disable",
             "institution",
@@ -74,36 +76,40 @@ class StudentInfoSerializer(serializers.ModelSerializer):
             "created_on",
         ]
 
-    def validate_date_of_birth(self, value):
-        if value > datetime.date.today():
-            raise serializers.ValidationError(
-                "Student date of birth must be a valid date"
-            )
+    def validate_dob(self, value):
+        if value > datetime.date.today() - datetime.timedelta(5400):
+            raise serializers.ValidationError("age should be greater than 15")
         return value
 
-    def validate_guardian_detail(self, guardian_detail):
-        if StudentInfo.objects.filter(guardian_detail__id=guardian_detail).exists():
+    def validate(self, phone):
+        if 10 > len(phone) > 13:
             raise serializers.ValidationError(
-                "Student with same guardian detail already exists"
+                "length of phone number should be greater than or equal to 10 and less than or equal to 13"
             )
+        return phone
 
     def create(self, validated_data):
         with transaction.atomic():
-            student_user = validated_data.pop("student_user")
-            student_serializer = UserSerializer(data=student_user)
+            user = validated_data.pop("user")
+            photo = validated_data.pop("photo")
+            student_serializer = UserSerializer(data=user)
             student_serializer.is_valid(raise_exception=True)
             student_user = student_serializer.save()
             student_user.general_info = validated_data.get("general_info")
             student = StudentInfo.objects.create(
                 **validated_data,
-                student_user=student_user,
+                user=user,
             )
-            student.student_user.username = student.admission_number
-            student_user.set_password(SYSTEM_DEFAULT_PASSWORD)
-            student.student_user.save()
+            student.user.username = student.admission_number
+            user.set_password(SYSTEM_DEFAULT_PASSWORD)
+            student.user.save()
+            if photo:
+                student.student_photo = to_internal_value(photo)
+                student.save()
             return student
 
     def update(self, instance, validated_data, *args, **kwargs):
+        photo = validated_data.pop("photo")
         with transaction.atomic():
             if validated_data.get("student_user"):
                 student_data = validated_data.pop("student_user")
@@ -112,4 +118,7 @@ class StudentInfoSerializer(serializers.ModelSerializer):
                 )
                 student_user_serializer.is_valid(raise_exception=True)
                 student_user_serializer.save()
+            if photo:
+                instance.photo = to_internal_value(photo)
+                instance.save()
             return super(StudentInfoSerializer, self).update(instance, validated_data)
