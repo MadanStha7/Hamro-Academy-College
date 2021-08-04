d00907e2a0a0e27381228a5676589537dde7858b
viperthapa <thaparam78666@gmail.com>
2021-08-04 14:30:18 +0545

    [add] staff,staff_academicinfo

--- staff/administrator/viewsets/staff.py
+++ staff/administrator/viewsets/staff.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from staff.models import Staff
-from staff.administrator.serializers.staff import StaffSerializer
+from staff.administrator.serializers.staff import StaffSerializer, StaffListSerializer
 from common.administrator.viewset import CommonInfoViewSet
 from django.db.models import F
+from rest_framework.response import Response
+from django.contrib.postgres.aggregates.general import ArrayAgg
 
 
 class StaffViewSet(CommonInfoViewSet):
@@ -17,3 +19,10 @@ class StaffViewSet(CommonInfoViewSet):
         queryset = Staff.objects.filter(institution=self.request.institution)
         queryset = queryset.annotate(designation__name=F("designation__name"))
         return queryset
+
+    def list(self, request):
+        """api to get list of serialzer of staff"""
+        queryset = Staff.objects.filter(institution=self.request.institution)
+        queryset = queryset.annotate(designation__name=F("designation__name"))
+        serializer = StaffListSerializer(queryset, many=True)
+        return Response(serializer.data)
