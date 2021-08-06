b2809fcc4384bb3e46d28a8da972a7d0c719390f
reebika <bhattareebika@gmail.com>
2021-08-06 17:40:41 +0545

    [Refactor] student document serializer

--- student/administator/serializer/document.py
+++ student/administator/serializer/document.py
@@ -8,19 +8,19 @@ from student.models import StudentDocument
 class StudentDocumentSerializer(serializers.ModelSerializer):
 
     document = serializers.CharField()
+    document_url = serializers.SerializerMethodField(required=False, allow_null=True)
 
-    def get_document_name(self, obj):
-        return StudentDocument.objects.filter(student=obj.student).values("document")
+    def get_document_url(self, obj):
+        return obj.document.url if obj.document else None
 
     class Meta:
         model = StudentDocument
         read_only_fields = ["institution", "created_by", "student"]
-        fields = ["id", "student", "document", "name"]
+        fields = ["id", "student", "document_url", "document", "name"]
 
     @transaction.atomic
     def create(self, validated_data):
         document = validated_data.pop("document")
-        print(validated_data)
         document = StudentDocument.objects.create(
                 document=to_internal_value(document), **validated_data)
 
