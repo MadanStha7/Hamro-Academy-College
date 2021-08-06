825c781ef11aee4d32c79225a75b9f0fc9069efa
reebika <bhattareebika@gmail.com>
2021-08-06 12:04:15 +0545

    [Add] previous academic serializer

---
+++ student/administator/serializer/previous_academic.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from student.models import PreviousAcademicDetail
+
+
+class PreviousAcademicSerializer(serializers.ModelSerializer):
+    """
+    serializer to list the previous student academic of student
+    """
+
+
+
+    class Meta:
+        model = PreviousAcademicDetail
+
+        read_only_fields = ["created_by", "institution"]
+        fields = [
+            "id",
+            "student",
+            "last_school",
+            "address",
+            "phone",
+            "email",
+            "institution",
+            "created_by"
+        ]
