b5d39a2989eefbce573cc7385d663839731236b0
ramthapa <thaparam78666@gmail.com>
2021-08-08 12:05:55 +0545

    [add] create timetable

--- timetable/administrator/utils/create_timetable.py
+++ timetable/administrator/utils/create_timetable.py
@@ -32,9 +32,10 @@ def create_timetable(infos, user, institution):
 
     # check the validation
     # valid_timetable = validate_timetable(infos,active_session,user, institution)
-
+    # print("valid timetable",valid_timetable)
     # for case of update
     for info in infos:
+        print("info", infos)
         get_id = info.get("id", None)
         if get_id:
             timetable = TimeTable(id=info.get("id"))
@@ -42,8 +43,13 @@ def create_timetable(infos, user, institution):
             timetable.faculty = Faculty(id=info.get("faculty"))
             timetable.grade = Grade(id=info.get("grade"))
             timetable.shift = Shift(id=info.get("shift"))
-            timetable.subject = Subject(id=info.get("subject"))
+            timetable.subject = Subject.objects.get(id=info.get("subject"))
+            timetable.day = (info.get("day"),)
+            timetable.start_time = (info.get("start_time"),)
+            timetable.end_time = (info.get("end_time"),)
+            print("dat1", User(id=info.get("teacher")))
             updated_timetable.append(timetable)
+
         # for case of create
         else:
             timetable = TimeTable(
@@ -59,13 +65,37 @@ def create_timetable(infos, user, institution):
                 institution=institution,
                 academic_session=active_session,
             )
+            print("grade in else", info.get("grade"))
+            print("teacher in else", info.get("teacher"))
+
             if info.get("section"):
                 timetable.section = info.get("section")
             new_timetables.append(timetable)
 
     with transaction.atomic():
+        print("updated timetable", updated_timetable)
         try:
             timetable_create = TimeTable.objects.bulk_create(new_timetables)
         except IntegrityError:
             raise ValidationError({"error": ["duplicate timetable is not allowed"]})
-        return {"timetable_create": timetable_create if timetable_create else []}
+
+        TimeTable.objects.bulk_update(
+            updated_timetable,
+            [
+                "teacher",
+                "start_time",
+                "end_time",
+                "day",
+                "subject",
+                "faculty",
+                "grade",
+                "shift",
+                "section",
+                "subject",
+                "academic_session",
+            ],
+        )
+        return {
+            "data1": timetable_create if timetable_create else [],
+            "data2": updated_timetable if updated_timetable else [],
+        }
