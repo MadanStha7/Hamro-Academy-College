5c89b30fe5d7de969cf37d4a8236421f17899de1
ramthapa <thaparam78666@gmail.com>
2021-08-08 11:53:13 +0545

    [add] implementation of base64 for image

--- staff/administrator/serializers/document.py
+++ staff/administrator/serializers/document.py
@@ -1,21 +1,29 @@
 from rest_framework import serializers
 from staff.models import Document
 from django.db import transaction
+from common.utils import to_internal_value
 
 
 class DocumentSerializer(serializers.ModelSerializer):
-    document = serializers.ListField(
-        child=serializers.FileField(max_length=100000), write_only=True
+
+    document = serializers.CharField()
+    document_name = serializers.SerializerMethodField(
+        read_only=True, required=False, allow_null=True
     )
+
+    def get_document_name(self, obj):
+        return obj.document.url if obj.document else None
 
     class Meta:
         model = Document
-        read_only_fields = ["institution", "created_by"]
-        fields = ["id", "staff", "document", "name"]
+        read_only_fields = ["institution", "created_by", "staff"]
+        fields = ["id", "staff", "document", "name", "document_name"]
 
     @transaction.atomic
     def create(self, validated_data):
+        print(validated_data)
         document = validated_data.pop("document")
-        for doc in document:
-            document = Document.objects.create(document=doc, **validated_data)
+        document = Document.objects.create(
+            document=to_internal_value(document), **validated_data
+        )
         return document
