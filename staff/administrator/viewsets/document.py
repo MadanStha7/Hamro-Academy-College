52d0f4149136d16dd0016a801955b28e86984a79
ramthapa <thaparam78666@gmail.com>
2021-08-08 11:59:20 +0545

    [add] document using base64

--- staff/administrator/viewsets/document.py
+++ staff/administrator/viewsets/document.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
-from staff.models import Document
+from staff.models import Document, Staff
 from staff.administrator.serializers.document import DocumentSerializer
 from common.administrator.viewset import CommonInfoViewSet
 from rest_framework.response import Response
+from rest_framework import status
 
 
 class DocumentViewSet(CommonInfoViewSet):
@@ -16,3 +17,21 @@ class DocumentViewSet(CommonInfoViewSet):
     def get_queryset(self):
         queryset = Document.objects.filter(institution=self.request.institution)
         return queryset
+
+    def perform_create(self, serializer):
+        staff = self.request.query_params.get("staff")
+        if self.request.institution:
+            serializer.save(
+                staff=Staff(id=staff),
+                created_by=self.request.user,
+                institution=self.request.institution,
+            )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
