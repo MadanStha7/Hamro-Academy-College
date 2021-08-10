3202555d2a7c5c411889f33a201ca3f81d8c0014
reebika <bhattareebika@gmail.com>
2021-08-10 16:41:53 +0545

    [Add] new filter

--- guardian/administrator/custom_filter.py
+++ guardian/administrator/custom_filter.py
@@ -5,9 +5,13 @@ from guardian.models import StudentGuardianInfo
 
 class StudentGuardianFilter(django_filters.rest_framework.FilterSet):
     student = django_filters.UUIDFilter(field_name='student_info__id', lookup_expr='exact')
+    section = django_filters.UUIDFilter(field_name='student_info__student_academic_detail__section__id',
+                                        lookup_expr='exact')
+    grade = django_filters.UUIDFilter(field_name='student_info__student_academic_detail__grade__id',
+                                      lookup_expr='exact')
 
     class Meta:
         model = StudentGuardianInfo
-        fields = ["student"]
+        fields = ["student", "grade", "section"]
 
 
