c34d2562d582b693a5593ddef0ceb09a9e570253
MadanStha7 <madanstha.124@gmail.com>
2021-08-10 15:02:56 +0545

    [fix] section in migrations

--- academics/migrations/0002_auto_20210809_1054.py
+++ academics/migrations/0002_auto_20210809_1054.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='applyshift',
             name='section',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='apply_shift', to='academics.section'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='apply_shift', to='academics.section'),
             preserve_default=False,
         ),
     ]
