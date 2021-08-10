3f28756afa3dda6ea67cfdcc2ea965b0d426750e
ramthapa <thaparam78666@gmail.com>
2021-08-10 15:37:35 +0545

    [add] change_password_urls

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,6 @@
 from user.administrator.urls.core import urlpatterns as role_urls
 from user.common.urls.core import urlpatterns as common_urls
+from user.common.urls.core import urlpatterns as change_password_urls
 
-urlpatterns = common_urls + role_urls
+
+urlpatterns = common_urls + role_urls + change_password_urls
