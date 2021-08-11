bee877defe660031d70ade5f308d30b5f913da24
reebika <bhattareebika@gmail.com>
2021-08-11 15:03:47 +0545

    [Add] subject group filter

---
+++ academics/custom_filters.py
@@ -0,0 +1,25 @@
+import django_filters
+
+from academics.models import SubjectGroup
+from timetable.models import TimeTable
+
+
+class SubjectGroupFilter(django_filters.rest_framework.FilterSet):
+    grade = django_filters.UUIDFilter(field_name='grade__id', lookup_expr='exact')
+    section = django_filters.UUIDFilter(field_name='section__id', lookup_expr='exact')
+    faculty = django_filters.UUIDFilter(field_name='faculty__id', lookup_expr='exact')
+    subject = django_filters.UUIDFilter(field_name='subject__id', lookup_expr='exact')
+
+    class Meta:
+        model = SubjectGroup
+        fields = ["grade", "section", "subject", "faculty"]
+
+
+class TeacherSubjectFilter(django_filters.rest_framework.FilterSet):
+    grade = django_filters.UUIDFilter(field_name='grade__id', lookup_expr='exact')
+
+
+    class Meta:
+        model = TimeTable
+        fields = ["grade"]
+
