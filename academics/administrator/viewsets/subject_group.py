38b36a08c01e89170fd819767d2bb5825ac6e687
reebika <bhattareebika@gmail.com>
2021-08-11 15:04:20 +0545

    [Add] subject group filter in viewset

--- academics/administrator/viewsets/subject_group.py
+++ academics/administrator/viewsets/subject_group.py
@@ -1,4 +1,7 @@
+from django_filters import rest_framework as filters
+
 from academics.administrator.serializers.subject_group import SubjectGroupSerializer
+from academics.custom_filters import SubjectGroupFilter
 from academics.models import SubjectGroup
 from common.administrator.viewset import CommonInfoViewSet
 
@@ -10,19 +13,9 @@ class SubjectGroupViewSet(CommonInfoViewSet):
 
     queryset = SubjectGroup.objects.none()
     serializer_class = SubjectGroupSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = SubjectGroupFilter
 
     def get_queryset(self):
-        grade = self.request.query_params.get("grade")
-        section = self.request.query_params.get("section")
-        faculty = self.request.query_params.get("faculty")
-        subject = self.request.query_params.get("subject")
         queryset = SubjectGroup.objects.filter(institution=self.request.institution)
-        if grade:
-            queryset = queryset.filter(grade__id=grade)
-        if faculty:
-            queryset = queryset.filter(faculty__id=faculty)
-        if subject:
-            queryset = queryset.filter(subject__id=subject)
-        if section:
-            queryset = queryset.filter(faculty__id=faculty)
         return queryset
