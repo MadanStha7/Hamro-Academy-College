3cf1debd1f8a2fe98bf9e9e9686f777237329232
reebika <bhattareebika@gmail.com>
2021-08-11 15:23:34 +0545

    [Refactor] subject_group viewset

--- academics/administrator/viewsets/subject_group.py
+++ academics/administrator/viewsets/subject_group.py
@@ -1,6 +1,8 @@
+from academics.administrator.custom_filter import SubjectGroupFilter
 from academics.administrator.serializers.subject_group import SubjectGroupSerializer
 from academics.models import SubjectGroup
 from common.administrator.viewset import CommonInfoViewSet
+from django_filters import rest_framework as filters
 
 
 class SubjectGroupViewSet(CommonInfoViewSet):
@@ -10,19 +12,10 @@ class SubjectGroupViewSet(CommonInfoViewSet):
 
     queryset = SubjectGroup.objects.none()
     serializer_class = SubjectGroupSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = SubjectGroupFilter
 
     def get_queryset(self):
-        grade = self.request.query_params.get("grade")
-        section = self.request.query_params.get("section")
-        faculty = self.request.query_params.get("faculty")
-        subject = self.request.query_params.get("subject")
         queryset = SubjectGroup.objects.filter(institution=self.request.institution)
-        if grade:
-            queryset = queryset.filter(grade__id=grade)
-        if faculty:
-            queryset = queryset.filter(faculty__id=faculty)
-        if subject:
-            queryset = queryset.filter(subject__id=subject)
-        if section:
-            queryset = queryset.filter(faculty__id=faculty)
+
         return queryset
