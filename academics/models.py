f98a52213a1c789123cc0ef70d6d46f8392f3a41
ramthapa <thaparam78666@gmail.com>
2021-08-13 12:08:39 +0545

    [fix] id changes

--- academics/models.py
+++ academics/models.py
@@ -31,7 +31,7 @@ class Grade(CommonInfo):
         return super(Grade, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.institution}-{self.name}"
+        return f"{self.name}"
 
 
 class Faculty(CommonInfo):
@@ -135,11 +135,19 @@ class Class(CommonInfo):
 
 
 class ApplyShift(CommonInfo):
-    shift = models.ForeignKey(Shift, related_name="apply_shift", on_delete=models.CASCADE)
+    shift = models.ForeignKey(
+        Shift, related_name="apply_shift", on_delete=models.CASCADE
+    )
     grade = models.ForeignKey(
         Grade, related_name="apply_shift", on_delete=models.CASCADE
     )
-    section = models.ForeignKey(Section, related_name="apply_shift", on_delete=models.CASCADE, blank=True, null=True)
+    section = models.ForeignKey(
+        Section,
+        related_name="apply_shift",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
     faculty = models.ForeignKey(
         Faculty, related_name="apply_shift", on_delete=models.CASCADE
     )
