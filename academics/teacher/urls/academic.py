1c4573e5a02bb14836f33a4522fdfc6fbd96589f
reebika <bhattareebika@gmail.com>
2021-08-18 16:32:20 +0545

    [Refactor] teacher url

--- academics/teacher/urls/academic.py
+++ academics/teacher/urls/academic.py
@@ -3,8 +3,8 @@ from rest_framework.urls import path
 from academics.teacher.viewsets.academic import GradeAPIView, FacultyAPIView, ShiftAPIView
 
 urlpatterns = [
-    path("my/grades/", GradeAPIView.as_view()),
-    path("my/faculty/", FacultyAPIView.as_view()),
-    path("my/shift/", ShiftAPIView.as_view()),
+    path("teacher/grades/", GradeAPIView.as_view()),
+    path("teacher/faculty/", FacultyAPIView.as_view()),
+    path("teacher/shift/", ShiftAPIView.as_view()),
 
 ]
