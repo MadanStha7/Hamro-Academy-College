58f3a9e8253059edfad0d41d0208e83ee916b33f
ramthapa <thaparam78666@gmail.com>
2021-08-18 15:02:58 +0545

    [fix] frontdeskofficer_urls for timetable

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,3 +1,5 @@
 from timetable.administrator.urls.core import urlpatterns as timetable_urls
+from timetable.frontdeskofficer.urls.core import urlpatterns as frontdeskofficer_urls
 
-urlpatterns = timetable_urls
+
+urlpatterns = timetable_urls + frontdeskofficer_urls
