9173e8bed90a9afe5abfaacc3b6a7bab93711699
ramthapa <thaparam78666@gmail.com>
2021-08-18 14:57:43 +0545

    [add] frontenddesk_urls

--- onlineclass/urls.py
+++ onlineclass/urls.py
@@ -1,3 +1,5 @@
 from onlineclass.administrator.urls.core import urlpatterns as administrator_urls
+from onlineclass.frontdeskofficer.urls.core import urlpatterns as frontdeskofficer_urls
 
-urlpatterns = administrator_urls
+
+urlpatterns = administrator_urls + frontdeskofficer_urls
