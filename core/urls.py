8cdf5cf5ad6e4008eec371dee50d5de0ad933b53
ramthapa <thaparam78666@gmail.com>
2021-08-18 14:55:41 +0545

    [add] frontdeskofficer_urls

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from .views import InstitutionInfoView
+from core.frontdeskofficer.urls.core import urlpatterns as frontdeskofficer_urls
 
-urlpatterns = [path("institution/", InstitutionInfoView.as_view(), name="institution")]
+
+urlpatterns = [
+    path("institution/", InstitutionInfoView.as_view(), name="institution")
+] + frontdeskofficer_urls
