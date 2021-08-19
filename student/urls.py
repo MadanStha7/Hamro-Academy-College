254337c09b53c5f264cc9535e080745043436d37
MadanStha7 <madanstha.124@gmail.com>
2021-08-19 12:51:00 +0545

    [add] student urls

--- student/urls.py
+++ student/urls.py
@@ -1,3 +1,4 @@
 from student.administator.urls.core import urlpatterns as administrator_urls
+from student.student.urls.core import urlpatterns as student_urls
 
-urlpatterns = administrator_urls
+urlpatterns = administrator_urls + student_urls
