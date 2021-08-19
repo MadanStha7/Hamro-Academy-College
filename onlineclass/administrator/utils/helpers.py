1563e5749ccfd00f8fcc8d658728c9ab5f8cc350
MadanStha7 <madanstha.124@gmail.com>
2021-08-19 12:50:23 +0545

    [add] date validator

---
+++ onlineclass/administrator/utils/helpers.py
@@ -0,0 +1,11 @@
+from rest_framework.exceptions import ValidationError
+import datetime
+
+
+def validate_date(value):
+    """
+    helper function to validate any date that should not accept past dates
+    """
+    if value < datetime.date.today():
+        raise ValidationError("past date is not accepted")
+    return value
