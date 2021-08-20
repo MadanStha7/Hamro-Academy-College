a7b03afa25912fa69fe5bfb81af9219a4b0bade1
MadanStha7 <madanstha.124@gmail.com>
2021-08-20 16:31:46 +0545

    [add] testcase for authentication

---
+++ authentication/tests/test_auth.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from common.tests.test_common import CommonTestCase
+
+
+class AuthTestCase(CommonTestCase):
+    """
+    test case to check if token get api is working
+    """
+
+    def setUp(self) -> None:
+        self.setup_common()
+
+    def test_get_token(self):
+        data = {"phone": self.get_created_by().phone, "password": "demo123"}
+        url = self.ADMINISTRATOR_URL + "token/"
+        res = self.client.post(path=url, data=data, format="json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        return res.data.get("token").get("access")
+
+    def get_token(self):
+        token = self.test_get_token()
+        return token
+
+    def change_role(self):
+        group = self.get_administrator_group()
+        self.get_created_by().groups.add(group)
+        self.get_created_by().save()
