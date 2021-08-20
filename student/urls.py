3de6a6b1fb940f3bca341b3d51f6481f1df72ef9
MadanStha7 <madanstha.124@gmail.com>
2021-08-20 10:59:45 +0545

    [add] student url

--- student/urls.py
+++ student/urls.py
@@ -1,5 +1,5 @@
 from student.administator.urls.core import urlpatterns as administrator_urls
-from student.student.urls.core import urlpatterns as student_urls
+from student.student.urls.core import urlpatterns as student_url
 from student.frontdeskofficer.urls.core import urlpatterns as frontdeskofficer_urls
 
-urlpatterns = administrator_urls + frontdeskofficer_urls + student_urls
+urlpatterns = administrator_urls + frontdeskofficer_urls + student_url
