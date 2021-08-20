412618a28eb1808ce25e5e95c67b97bb14e22fe8
MadanStha7 <madanstha.124@gmail.com>
2021-08-20 10:47:50 +0545

    [add] api for student profile

--- student/urls.py
+++ student/urls.py
@@ -1,4 +1,5 @@
 from student.administator.urls.core import urlpatterns as administrator_urls
 from student.student.urls.core import urlpatterns as student_urls
+from student.frontdeskofficer.urls.core import urlpatterns as frontdeskofficer_urls
 
-urlpatterns = administrator_urls + student_urls
+urlpatterns = administrator_urls + frontdeskofficer_urls + student_urls
