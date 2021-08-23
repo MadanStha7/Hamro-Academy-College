3330845c9dabebfbdd6db2b82ceaa6b6e735d708
reebika <bhattareebika@gmail.com>
2021-08-23 13:30:39 +0545

    [Add] new model in fees

--- fees/models.py
+++ fees/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from academics.models import Faculty, Grade
+from common.constant import DISCOUNT_FINE_OPTIONS
 from common.models import CommonInfo
 
 
@@ -24,3 +25,59 @@ class FeeType(CommonInfo):
     class Meta:
         ordering = ["-created_on"]
         db_table = "fee_type"
+
+
+class DiscountType(CommonInfo):
+    """
+    Discount method information
+    """
+
+    name = models.CharField(max_length=50)
+    discount_mode = models.CharField(
+        max_length=1,
+        choices=DISCOUNT_FINE_OPTIONS,
+    )
+    discount_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    def __str__(self):
+        return "{} -- {}".format(self.name, self.institution)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        return super(DiscountType, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "discount_type"
+        verbose_name = "Discount Type"
+        verbose_name_plural = "Discount Types"
+
+
+class FineType(CommonInfo):
+    """
+    Fine method information
+    """
+
+    name = models.CharField(max_length=50)
+    fine_mode = models.CharField(
+        max_length=1,
+        choices=DISCOUNT_FINE_OPTIONS,
+    )
+    fine_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    def __str__(self):
+        return "{} -- {}".format(self.name, self.institution)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        return super(FineType, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "fine_type"
+        verbose_name = "Fine Type"
+        verbose_name_plural = "Fine Types"
