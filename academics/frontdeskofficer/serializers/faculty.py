e2d2a1423916252ddac54148693175dabb1afdb7
ramthapa <thaparam78666@gmail.com>
2021-08-24 13:09:18 +0545

    [fix] code refactor

--- academics/frontdeskofficer/serializers/faculty.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from academics.models import Faculty
-
-
-class FacultySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Faculty
-        read_only_fields = ["institution", "created_by"]
-        fields = ["id", "name", "created_by", "institution"]
