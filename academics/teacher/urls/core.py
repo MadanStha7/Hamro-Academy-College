1a4e617b71185681498ec521ff222b669584ae4c
ramthapa <thaparam78666@gmail.com>
2021-08-24 13:23:53 +0545

    [fix] code refactor

--- academics/teacher/urls/core.py
+++ academics/teacher/urls/core.py
@@ -5,7 +5,4 @@ from academics.teacher.urls.profile import urlpatterns as profile_urls
 from academics.teacher.urls.online_class import urlpatterns as online_class
 
 
-urlpatterns = (
-    subject_urls + academic_urls + timetable_urls + profile_urls 
-
-)
+urlpatterns = subject_urls + academic_urls + timetable_urls + profile_urls
