664c7f408434e2b6ec8959947c53a2a03a94dfd2
reebika <bhattareebika@gmail.com>
2021-08-29 17:15:51 +0545

    [Add] searches in subject_group

--- academics/administrator/viewsets/subject_group.py
+++ academics/administrator/viewsets/subject_group.py
@@ -2,20 +2,19 @@ from academics.administrator.custom_filter import SubjectGroupFilter
 from academics.administrator.serializers.subject_group import SubjectGroupSerializer
 from academics.models import SubjectGroup
 from common.administrator.viewset import CommonInfoViewSet
-from django_filters import rest_framework as filters
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class SubjectGroupViewSet(CommonInfoViewSet):
     """
     CRUD for Subject Group .
     """
-
-
-
     queryset = SubjectGroup.objects.none()
     serializer_class = SubjectGroupSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     filter_class = SubjectGroupFilter
+    search_fields = ["grade__name", "faculty__name", "section__name"]
 
     def get_queryset(self):
         queryset = SubjectGroup.objects.filter(institution=self.request.institution)
