e2a22229223b92765451f52aa8bb6159297bdacf
ramthapa <thaparam78666@gmail.com>
2021-08-30 18:06:01 +0545

    [fix] delete unnecessary files

--- onlineclass/frontdesk/urls/core.py
+++ onlineclass/frontdesk/urls/core.py
@@ -1,5 +1,3 @@
 from .online_class import urlpatterns as online_class_urls
-from .online_class_filters import urlpatterns as online_class_filters_urls
 
-
-urlpatterns = online_class_urls + online_class_filters_urls
+urlpatterns = online_class_urls
--- onlineclass/frontdesk/urls/online_class_filters.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.urls import path
-from onlineclass.frontdesk.views.online_class_filters import (
-    OnlineClassFilterListAPIView,
-)
-
-
-urlpatterns = [
-    path("frontdesk/online-class-filter/", OnlineClassFilterListAPIView.as_view()),
-]
