dab4bf5685efe631b075b9cd668623c202d779d7
ramthapa <thaparam78666@gmail.com>
2021-08-30 18:05:23 +0545

    [add] filters add in online class

--- academics/frontdesk/views/class_url.py
+++ academics/frontdesk/views/class_url.py
@@ -1,10 +1,14 @@
 from django.db.models import F
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
-from academics.frontdesk.serializers.class_url import ClassSerializer
+from academics.frontdesk.serializers.class_url import (
+    ClassSerializer,
+    OnlineclassFilterSerializer,
+)
 from permissions.front_desk_officer import FrontDeskPermission
 from rest_framework.serializers import ValidationError
 from academics.models import Class
+from rest_framework.response import Response
 
 
 class ClassListAPIView(ListAPIView):
@@ -16,10 +20,33 @@ class ClassListAPIView(ListAPIView):
 
     def get_queryset(self):
         queryset = Class.objects.filter(institution=self.request.institution)
-        queryset = queryset.annotate(
-            faculty_name=F("faculty__name"), grade_name=F("grade__name")
-        )
         return queryset
+
+    def list(self, request, *args, **kwargs):
+        """api to get list of grade"""
+        faculty = self.request.query_params.get("faculty")
+        if faculty:
+            queryset = (
+                Class.objects.filter(
+                    faculty__id=faculty, institution=self.request.institution
+                )
+                .values("grade__name", "grade__id", "section__id", "section__name")
+                .distinct()
+            )
+            serialzer = OnlineclassFilterSerializer(queryset, many=True)
+            return Response(serialzer.data)
+        else:
+            institution = self.request.institution
+            queryset = self.get_queryset()
+            queryset = self.filter_queryset(queryset)
+            page = self.paginate_queryset(queryset)
+            page_ids = [i.id for i in page]
+            result = queryset.filter(id__in=page_ids)
+            result = result.annotate(
+                faculty_name=F("faculty__name"), grade_name=F("grade__name")
+            )
+            serializer = ClassSerializer(result, many=True)
+            return self.get_paginated_response(serializer.data)
 
 
 class ClassRetrieveAPIView(RetrieveAPIView):
