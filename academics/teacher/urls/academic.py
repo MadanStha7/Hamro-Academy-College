632dbbf6e2de31a660044913c0c51082ca546225
reebika <bhattareebika@gmail.com>
2021-08-31 12:16:20 +0545

    [Refactor] teacher url

--- academics/teacher/urls/academic.py
+++ academics/teacher/urls/academic.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
+from rest_framework import routers
 
-from academics.teacher.viewsets.academic import (
-    GradeAPIView,
-    FacultyAPIView,
-    ShiftAPIView,
-)
+from academics.teacher.views.academic import GradeAPIView, FacultyAPIView, ShiftAPIView
 
-teacher_urlpatterns = [
-    path("grades/", GradeAPIView.as_view()),
-    path("faculty/", FacultyAPIView.as_view()),
-    path("shift/", ShiftAPIView.as_view()),
-]
+router = routers.DefaultRouter()
 
+
+teacher_urlpatterns = [path("", include(router.urls)),
+               path("grades/", GradeAPIView.as_view()),
+               path("faculty/", FacultyAPIView.as_view()),
+               path("shift/", ShiftAPIView.as_view()),
+               ]
 urlpatterns = [path("teacher/", include(teacher_urlpatterns))]
