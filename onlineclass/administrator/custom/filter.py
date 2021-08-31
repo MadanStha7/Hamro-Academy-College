6540ccbb4da8cafde023eab8b59b59404176898d
MadanStha7 <madanstha.124@gmail.com>
2021-08-31 13:56:29 +0545

    [add] fields in filter

--- onlineclass/administrator/custom/filter.py
+++ onlineclass/administrator/custom/filter.py
@@ -15,11 +15,22 @@ class OnlineClassFilter(django_filters.rest_framework.FilterSet):
 
 class OnlineClassAttendanceFilter(django_filters.rest_framework.FilterSet):
     days = django_filters.CharFilter(lookup_expr="icontains")
-    grade = django_filters.UUIDFilter(field_name="grade__id", lookup_expr="exact")
-    section = django_filters.UUIDFilter(field_name="section__id", lookup_expr="exact")
-    faculty = django_filters.UUIDFilter(field_name="faculty__id", lookup_expr="exact")
+    grade = django_filters.UUIDFilter(
+        field_name="online_class__grade__id", lookup_expr="exact"
+    )
+    section = django_filters.UUIDFilter(
+        field_name="online_class__section__id", lookup_expr="exact"
+    )
+    faculty = django_filters.UUIDFilter(
+        field_name="online_class__faculty__id", lookup_expr="exact"
+    )
+    online_class = django_filters.UUIDFilter(
+        field_name="online_class__id", lookup_expr="exact"
+    )
+    subject = django_filters.UUIDFilter(
+        field_name="online_class__subject__id", lookup_expr="exact"
+    )
 
     class Meta:
         model = StudentOnlineClassAttendance
-        fields = ["days", "grade", "section", "faculty"]
-
+        fields = ["days", "grade", "section", "faculty", "online_class", "subject"]
