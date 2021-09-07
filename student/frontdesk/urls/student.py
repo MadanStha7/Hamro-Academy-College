a8f2f65e5bd621423e95de9d96fde21fbbfdf8b5
ramthapa <thaparam78666@gmail.com>
2021-09-07 15:59:16 +0545

    [add] student disable api

--- student/frontdesk/urls/student.py
+++ student/frontdesk/urls/student.py
@@ -2,9 +2,11 @@ from rest_framework.urls import path
 from student.frontdesk.views.student import (
     StudentListAPIView,
     StudentRetrieveAPIView,
+    StudentDisableAPIView,
 )
 
 urlpatterns = [
     path("frontdesk/student/", StudentListAPIView.as_view()),
     path("frontdesk/student/<pk>/", StudentRetrieveAPIView.as_view()),
+    path("frontdesk/student-disable/", StudentDisableAPIView.as_view()),
 ]
